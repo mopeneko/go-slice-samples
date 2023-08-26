c117ee2d3ac9d1981d777aa5c11eda243d3c837c
mopeneko <mopeneko@lem0n.cc>
2023-08-26 15:58:43 +0900

    Fix code and output

--- append_element.go
+++ append_element.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 	s1 := make([]int, 0, 1)
-	PrintSliceInfo(s1) // [] | len=0 cap=1 ptr=0xc0000a8000
+	PrintSliceInfo(s1) // [] | len=0 cap=1 ptr=0xc0000a2000
 	s1 = append(s1, 1)
-	PrintSliceInfo(s1) // [1] | len=1 cap=1 ptr=0xc0000a8000
+	PrintSliceInfo(s1) // [1] | len=1 cap=1 ptr=0xc0000a2000
 
 	s2 := make([]int, 0)
-	PrintSliceInfo(s1) // [1] | len=1 cap=1 ptr=0xc0000a8000
+	PrintSliceInfo(s2) // [] | len=0 cap=0 ptr=0x54e3a0
 	s2 = append(s2, 1)
-	PrintSliceInfo(s2) // [1] | len=1 cap=1 ptr=0xc0000a8030
+	PrintSliceInfo(s2) // [1] | len=1 cap=1 ptr=0xc0000a2028
 }
 
 func PrintSliceInfo[T any](s []T) {
